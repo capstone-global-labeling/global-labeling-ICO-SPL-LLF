e4e4b86f230877b2250a62dd4c3921bc41ef34b9
Jason <Jason@mac.mynetworksettings.com>
2025-03-28 23:57:22 -0400

    fix: update to backend to parse through list of establishment prefixes
    Note that the [1] index on establishments_list is due to parsing through the list of prefixes and not the mapping of the full establishment to its address (establishments_list[0])

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from read_in_establishments import read_establishments_as_list, create_search_li
 
 excel_file = input("Enter excel file here: ")
 establishments_list = read_establishments_as_list(excel_file)
-print(create_search_links(establishments_list))
+print(create_search_links(establishments_list[1]))
 
